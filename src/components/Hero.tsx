9e920ea57c5c80bf08fd38d396a7ab06262107a2
Siddharth <siddharth17vaishnav@gmail.com>
2023-09-02 16:25:28 +0530

    title animation

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,4 +1,16 @@
 import { Box, Container, Grid, Typography } from "@mui/material";
+import { Tilt } from "react-tilt";
+const defaultOptions = {
+  reverse: false, // reverse the tilt direction
+  max: 35, // max tilt rotation (degrees)
+  perspective: 1000, // Transform perspective, the lower the more extreme the tilt gets.
+  scale: 1.1, // 2 = 200%, 1.5 = 150%, etc..
+  speed: 1000, // Speed of the enter/exit transition
+  transition: true, // Set a transition on enter/exit.
+  axis: null, // What axis should be disabled. Can be X or Y.
+  reset: true, // If the tilt effect has to be reset on exit.
+  easing: "cubic-bezier(.03,.98,.52,.99)", // Easing on enter/exit.
+};
 
 const Hero = () => {
   return (
@@ -74,11 +86,13 @@ const Hero = () => {
               }}
               mt={{ xs: 5, md: 0 }}
             >
-              <img
-                src="/code.gif"
-                alt="CODE"
-                style={{ borderRadius: "26px", overflow: "hidden" }}
-              />
+              <Tilt options={defaultOptions}>
+                <img
+                  src="/code.gif"
+                  alt="CODE"
+                  style={{ borderRadius: "26px", overflow: "hidden" }}
+                />
+              </Tilt>
             </Grid>
           </Grid>
         </Container>
